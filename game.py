01899ed1efce24913b71336dd7cebb1e5fb965c7
tanya98 <128077502+tanyusha98@users.noreply.github.com>
2026-02-09 00:24:36 +0300

    add guess check logic

--- game.py
+++ game.py
@@ -5,3 +5,12 @@ def generate_number():
     return random.randint(1, 10)
 
 print("Guess the number game")
+import random
+
+def generate_number():
+    return random.randint(1, 10)
+
+def check_guess(secret, guess):
+    return secret == guess
+
+print("Guess the number game")
